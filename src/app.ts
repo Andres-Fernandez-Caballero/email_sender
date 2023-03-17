d934cf9da2c4a76274305060f1680c5c01a08cd1
Andres Fernandez <andres.fernandezcaballero@davinci.edu.ar>
2023-03-17 14:38:31 -0300

    ADD cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import express from 'express';
 import routes from "@src/routes";
+import cors, {CorsOptions} from 'cors';
 
 const app = express();
 
+
+const corsOptions: CorsOptions = {
+    origin: '*',
+}
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
